581fd879545075b0d9fd3311162ded8236c4594f
MikolajMichalczak <michalczakmikolaj@gmail.com>
2024-01-21 14:46:49 +0100

    create events pager with tabs

--- app/src/main/java/com/example/gatherpoint/ui/events/MyEventsFragment.kt
+++ app/src/main/java/com/example/gatherpoint/ui/events/MyEventsFragment.kt
@@ -1,15 +1,15 @@
-package com.example.gatherpoint.ui
+package com.example.gatherpoint.ui.events
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.gatherpoint.databinding.FragmentEventsBinding
+import com.example.gatherpoint.databinding.FragmentMyEventsBinding
 
-class EventsFragment: Fragment() {
+class MyEventsFragment: Fragment() {
 
-    private var _binding: FragmentEventsBinding? = null
+    private var _binding: FragmentMyEventsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -17,18 +17,21 @@ class EventsFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentEventsBinding.inflate(inflater, container, false)
+        _binding = FragmentMyEventsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        fun newInstance() = MyEventsFragment()
     }
 
 }
